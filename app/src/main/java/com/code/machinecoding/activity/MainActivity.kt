73acbfb128b7d5a8b8a6d6df3f213ea6d2c18533
sounak saha <sounaksaha100@gmail.com>
2026-01-03 17:00:34 +0530

    initial commit

---
+++ app/src/main/java/com/code/machinecoding/activity/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.code.machinecoding.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.code.machinecoding.ui.theme.MachineCodingTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MachineCodingTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Greeting("Machine Coding Test")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello, $name!")
+}
+
